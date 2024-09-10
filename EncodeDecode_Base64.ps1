d8653c470e496db2e6d94fdc99dc2931b795746b
aenagy <aenagy@hotmail.com>
2024-09-10 13:26:01 -0400

    Initial commit.

---
+++ EncodeDecode_Base64.ps1
@@ -0,0 +1,103 @@
+<#
+I need a pure PowerShell solution to encode a binary file to base 64 and to decode base64 to a binary file.
+
+Inspired by the following Stack Overflow article:
+
+* Encode / Decode .EXE into Base64
+  `https://stackoverflow.com/questions/42592518/encode-decode-exe-into-base64`
+
+... and the answer from bielawski (`https://stackoverflow.com/users/2121227/bielawski`):
+
+* `https://stackoverflow.com/a/60671753`
+
+#>
+
+Param(
+    [Parameter(Mandatory=$true)]
+    [ValidateScript(
+        {Test-Path -Path $_ -PathType Leaf}
+    )]
+    [string]
+    $inFile
+    ,
+    [string]
+    $outFile
+    ,
+    [Parameter(Mandatory=$true)]
+    [ValidateSet( "encode", "en", "decode", "de" )]
+    [string]
+    $action
+)
+
+$fileExtensionDivider = "."
+$folderPathDivider = "\"
+$inputFile = Get-ChildItem -Path $inFile
+$action = $action.Substring( 0 , 2 )
+
+if ( Test-Path -Path $inputFile -PathType Leaf )
+{
+    Write-Host "Found inFile '${infile}'. Continuing."
+} else {
+    Write-Error "ERROR: Unable to find 'inFile' '${inFile}'. Aborting script."
+    exit 1
+}
+
+if ( $outFile -and $outFile.Length -gt 0)
+{
+    if ( Split-Path -Path $outFile -IsAbsolute )
+    {
+        [string]$outputFileFolder    = $inputFile.DirectoryName
+
+    } else {
+        # https://superuser.com/questions/1660757/how-to-get-current-path-in-powershell-into-a-variable
+        [string]$outputFileFolder    = $PWD.Path
+    }
+    # https://stackoverflow.com/a/9788998
+    [string]$outputFileBaseName      = [System.IO.Path]::GetFileNameWithoutExtension( $outFile )
+    [string]$outputFileExtension     = [System.IO.Path]::GetExtension( $outFile ).Replace( $fileExtensionDivider , "" )
+} else {
+    Write-Information "`$outFile is either null nor empty. Using 'inFile' name for 'outFile'. Continuing."
+    # https://stackoverflow.com/a/9788998
+    [string]$outputFileFolder        = $inputFileList[0].DirectoryName
+    [string]$outputFileBaseName      = [System.IO.Path]::GetFileNameWithoutExtension( $inFile  ).Replace( "*" , "" ).Replace( "?" , "" )
+    [string]$outputFileExtension     = [System.IO.Path]::GetExtension( $inFile ).Replace( "*" , "" ).Replace( "?" , "" ).Replace( $fileExtensionDivider , "" )
+}
+
+
+if ( Test-Path -Path $outputFile )
+{
+    Write-Error "`nERROR: output file '$outputFile' exists. Delete or rename this file and retry. Aborting script.`n"
+    exit 2
+}
+Write-Information "Output file            '$outputFile' does not exist. Continuing."
+
+
+switch ( $action )
+{
+    "en" {
+        $outputFile = $outputFileFolder + $folderPathDivider + $outputFileBaseName + $fileExtensionDivider + $outputFileExtension 
+        $outFileEncoded = $outputFile + ".b64"
+        # New-Item -Path $outFileEncoded -ItemType File
+        Write-Host "Encoding from input file '$inputFile.FullName' to output file '$outFileEncoded'."
+        [IO.File]::WriteAllBytes( $outFileEncoded , [char[]][Convert]::ToBase64String([IO.File]::ReadAllBytes($inputFile.FullName)) )
+        break
+
+    }
+    "de" {
+        if ( $outputFileExtension -eq "b64" )
+        {
+            $outputFile = $outputFileFolder + $folderPathDivider + $outputFileBaseName
+
+        } else {
+            $outputFile = $outputFileFolder + $folderPathDivider + $outputFileBaseName + $fileExtensionDivider + $outputFileExtension 
+
+        }
+        $outFileDecoded = $outputFile
+        # New-Item -Path $outFileDecoded -ItemType File
+        Write-Host "Decoding from input file '$inputFile.FullName' to output file '$outFileEncoded'."
+        [IO.File]::WriteAllBytes( $outFileDecoded , [Convert]::FromBase64String([char[]][IO.File]::ReadAllBytes($inputFile.FullName)) )
+        break
+    }
+}
+
+Write-Host "Done."
--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # encode_decode_base64_PowerShell5
+
+## Introduction
+
+I need a pure PowerShell solution to encode a binary file to base 64 and to decode base64 to a binary file.
+
+Inspired by the following Stack Overflow article:
+
+* Encode / Decode .EXE into Base64
+  `https://stackoverflow.com/questions/42592518/encode-decode-exe-into-base64`
+
+... and the answer from bielawski (`https://stackoverflow.com/users/2121227/bielawski`):
+
+* `https://stackoverflow.com/a/60671753`
+
+
+## Usage
+
+This PowerShell script was created and tested with version 5.1 on Windows 10. This version of PowerShell was chosen as some environments don't allow users to install/upgrade to PowerShell 7+.
+
+If/when/maybe I have time I will test on other platforms such as Ubuntu.
+
+## Execution
+
+These examples asume the above PowerShell session. Note that the script requires that the output file does NOT exist.
+
+The 'action' parameter will accpent "en" or "encode" for base64 encoding and "de" or "decode" for base64 decoding.
+
+### Encode a binary file to base64 text file.
+
+If the output file is not specified then it is the same as the input file name with ".b64" appended as the new extension.
+
+`.\EncodeDecode_Base64.ps1 -inFile .\testfile.zip -action en`
+
+### Encode a binary file to base64 text file specified by user.
+
+`.\EncodeDecode_Base64.ps1 -inFile .\testfile.zip -action en -outFile foobar`
+
+### Decode a base64 text file to binary file.
+
+`.\EncodeDecode_Base64.ps1 -inFile .\foobar..b64 -outFile barfoo.zip -action decode`
+
+
+## Alternate solution for small-ish files
+
+Use the Windows built-in utility 'certutil'.
+
+`Get-Content -Path textfile??.txt | Out-File -FilePath foobar.txt -Append`
+
+## Author
+
+Andrew Nagy
+
+https://www.linkedin.com/in/andrew-e-nagy/
