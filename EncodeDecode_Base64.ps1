3d1c8523c2bdcfd23fcb075e9d96fb73949b0dc0
anagy.adm <Andrew.Nagy@convergetp.com>
2024-11-21 23:00:28 -0500

    fixed faulty logic the prevented encoding. added hash output.

--- EncodeDecode_Base64.ps1
+++ EncodeDecode_Base64.ps1
@@ -36,11 +36,12 @@ $action = $action.Substring( 0 , 2 )
 
 if ( Test-Path -Path $inputFile -PathType Leaf )
 {
-    Write-Host "Found inFile '${infile}'. Continuing."
+    Write-Host "Found inFile     '${infile}'. Continuing."
 } else {
     Write-Error "ERROR: Unable to find 'inFile' '${inFile}'. Aborting script."
     exit 1
 }
+$inFileObj = Get-ChildItem -Path $inFile
 
 if ( $outFile -and $outFile.Length -gt 0)
 {
@@ -58,30 +59,34 @@ if ( $outFile -and $outFile.Length -gt 0)
 } else {
     Write-Information "`$outFile is either null nor empty. Using 'inFile' name for 'outFile'. Continuing."
     # https://stackoverflow.com/a/9788998
-    [string]$outputFileFolder        = $inputFileList[0].DirectoryName
+    [string]$outputFileFolder        = $PWD.Path
     [string]$outputFileBaseName      = [System.IO.Path]::GetFileNameWithoutExtension( $inFile  ).Replace( "*" , "" ).Replace( "?" , "" )
     [string]$outputFileExtension     = [System.IO.Path]::GetExtension( $inFile ).Replace( "*" , "" ).Replace( "?" , "" ).Replace( $fileExtensionDivider , "" )
 }
 
 
+$outputFile = Join-Path -Path $outputFileFolder -ChildPath ( $outputFileBaseName + "." + $outputFileExtension )
+<#
 if ( Test-Path -Path $outputFile )
 {
     Write-Error "`nERROR: output file '$outputFile' exists. Delete or rename this file and retry. Aborting script.`n"
     exit 2
 }
 Write-Information "Output file            '$outputFile' does not exist. Continuing."
+#>
 
 
 switch ( $action )
 {
     "en" {
-        $outputFile = $outputFileFolder + $folderPathDivider + $outputFileBaseName + $fileExtensionDivider + $outputFileExtension 
+        #$outputFile = $outputFileFolder + $folderPathDivider + $outputFileBaseName + $fileExtensionDivider + $outputFileExtension 
         $outFileEncoded = $outputFile + ".b64"
         # New-Item -Path $outFileEncoded -ItemType File
-        Write-Host "Encoding from input file '$inputFile.FullName' to output file '$outFileEncoded'."
+        Write-Host "Encoding:`n from input file '$($inputFile.FullName)'`n to output file  '$outFileEncoded'."
         [IO.File]::WriteAllBytes( $outFileEncoded , [char[]][Convert]::ToBase64String([IO.File]::ReadAllBytes($inputFile.FullName)) )
+        #Get-ChildItem -Path $outputFile
+        Get-FileHash -Algorithm SHA384 -Path $outputFile | Format-List
         break
-
     }
     "de" {
         if ( $outputFileExtension -eq "b64" )
@@ -94,8 +99,10 @@ switch ( $action )
         }
         $outFileDecoded = $outputFile
         # New-Item -Path $outFileDecoded -ItemType File
-        Write-Host "Decoding from input file '$inputFile.FullName' to output file '$outFileEncoded'."
+        Write-Host "Decoding:`n from input file '$inputFile.FullName'`n to output file  '$outFileEncoded'."
         [IO.File]::WriteAllBytes( $outFileDecoded , [Convert]::FromBase64String([char[]][IO.File]::ReadAllBytes($inputFile.FullName)) )
+        #Get-ChildItem -Path $outputFile
+        Get-FileHash -Algorithm SHA384 -Path $outputFile | Format-List
         break
     }
 }
